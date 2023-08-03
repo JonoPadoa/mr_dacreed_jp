a9612a671b927751975fff3057bb8ac8c033206c
JonoPadoa <jonathanp@missionreadyhq.com>
2023-08-04 10:42:23 +1200

    Moved db query to JonosPage2 from home page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -46,11 +46,6 @@ const Home: NextPage = () => {
             <div className="m-8">
               <AuthShowcase />
             </div>
-            <div>
-              {" "}
-              <Header />
-              <Content />
-            </div>
           </div>
         </div>
       </main>
@@ -83,72 +78,72 @@ export { AuthShowcase };
 // type Topic = RouterOutputs["topic"]["getAll"] extends Array<infer T> ? T : never;
 // previouse line is replaced with the next one:
 
-type Topic = {
-  id: string;
-  createdAt: Date;
-  updatedAt: Date;
-  title: string;
-  userId: string;
-};
+// type Topic = {
+//   id: string;
+//   createdAt: Date;
+//   updatedAt: Date;
+//   title: string;
+//   userId: string;
+// };
 
-const Content: React.FC = () => {
-  const [selectedTopic, setSelectedTopic] = useState<Topic | null>(null);
-  const { data: topics, refetch: refetchTopics } = api.topic.getAll.useQuery(
-    undefined, // no input
-    {
-      onSuccess: (data) => {
-        if (data && data.length > 0 && data[0]) {
-          setSelectedTopic(data[0]);
-        } else {
-          setSelectedTopic(null);
-        }
-      },
-    }
-  );
+// const Content: React.FC = () => {
+//   const [selectedTopic, setSelectedTopic] = useState<Topic | null>(null);
+//   const { data: topics, refetch: refetchTopics } = api.topic.getAll.useQuery(
+//     undefined, // no input
+//     {
+//       onSuccess: (data) => {
+//         if (data && data.length > 0 && data[0]) {
+//           setSelectedTopic(data[0]);
+//         } else {
+//           setSelectedTopic(null);
+//         }
+//       },
+//     }
+//   );
 
-  const createTopic = api.topic.create.useMutation({
-    onSuccess: () => {
-      void refetchTopics();
-    },
-  });
+//   const createTopic = api.topic.create.useMutation({
+//     onSuccess: () => {
+//       void refetchTopics();
+//     },
+//   });
 
-  return (
-    <div className="mx-5 mt-5 grid grid-cols-4 gap-2">
-      <div className="px-2">
-        <ul className="menu rounded-box w-56 bg-base-100 p-2">
-          {topics?.map((topic) => (
-            <li key={topic.id}>
-              <a
-                href="#"
-                onClick={(evt) => {
-                  evt.preventDefault();
-                  setSelectedTopic(topic);
-                }}
-              >
-                {topic.title}
-              </a>
-            </li>
-          ))}
-        </ul>
-        <div className="divider"></div>
-        <input
-          type="text"
-          placeholder="New Topic"
-          className="input-bordered input input-sm w-full"
-          onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              createTopic.mutate({
-                title: e.currentTarget.value,
-              });
-              e.currentTarget.value = "";
-            }
-          }}
-        />
-      </div>
-      <div className="col-span-3">
-        {/* Content goes here */}
-        {selectedTopic && <div>Selected topic: {selectedTopic.title}</div>}
-      </div>
-    </div>
-  );
-};
+//   return (
+//     <div className="mx-5 mt-5 grid grid-cols-4 gap-2">
+//       <div className="px-2">
+//         <ul className="menu rounded-box w-56 bg-base-100 p-2">
+//           {topics?.map((topic) => (
+//             <li key={topic.id}>
+//               <a
+//                 href="#"
+//                 onClick={(evt) => {
+//                   evt.preventDefault();
+//                   setSelectedTopic(topic);
+//                 }}
+//               >
+//                 {topic.title}
+//               </a>
+//             </li>
+//           ))}
+//         </ul>
+//         <div className="divider"></div>
+//         <input
+//           type="text"
+//           placeholder="New Topic"
+//           className="input-bordered input input-sm w-full"
+//           onKeyDown={(e) => {
+//             if (e.key === "Enter") {
+//               createTopic.mutate({
+//                 title: e.currentTarget.value,
+//               });
+//               e.currentTarget.value = "";
+//             }
+//           }}
+//         />
+//       </div>
+//       <div className="col-span-3">
+//         {/* Content goes here */}
+//         {selectedTopic && <div>Selected topic: {selectedTopic.title}</div>}
+//       </div>
+//     </div>
+//   );
+// };
--- src/pages/namedLibrary/namePage/jonosPage2.tsx
+++ src/pages/namedLibrary/namePage/jonosPage2.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { api, type RouterOutputs } from "~/utils/api";
+import { useState } from "react";
 const jonosPage2 = () => {
   return (
     <div>
-      <h1>PLACEHOLDER</h1>
+      <h1>Placeholder</h1>
     </div>
   );
 };
